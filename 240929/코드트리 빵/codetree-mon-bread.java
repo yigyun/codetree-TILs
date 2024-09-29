9e1e56673ff987f0497721c93afa1d6ecd8327cd
yigyun <70265528+yigyun@users.noreply.github.com>
2024-09-29 13:50:11 +0900

    Title: 코드트리 빵, Time: 299ms, Memory: 14MB, Status: Passed - CodeTree

--- 240929/코드트리 빵/codetree-mon-bread.java
+++ 240929/코드트리 빵/codetree-mon-bread.java
@@ -27,6 +27,7 @@ public class Main {
     static People[] peoples; // 사람 정보, 앞에는 사람 번호, 뒤에는 정보 x, y, 목표 좌표 x, y
     static boolean[][] visited;
     static boolean[] arrive;
+    static int count;
 
     static class People{
         int x; int y;
@@ -45,7 +46,8 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        
+
+        count = 0;
         map = new int[n][n];
         for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
@@ -66,7 +68,7 @@ public class Main {
         // 이제 맵에서 2 또는 1이 베이스캠프임.
         int k = 0;
         boolean check = true;
-        while(check){
+        while(count < m){
             //1. 본인이 가고 싶은 편의점 방향으로 1칸 움직임. 상 좌 우 하의 우선 순위로 움직인다.
             //도달하기 까지 거쳐야 하는 칸의 수가 최소가 되는 거리를 뜻한다. -> 매번 경로 길이를 알아야 한다는 거인듯
             move(k);
@@ -77,7 +79,8 @@ public class Main {
             //자신이 가고 싶은 편의점과 가장 가까이 있는 베이스 캠프에 들어간다. 겹치면 행이 작은, 열이 작은 순서로
             //3번까지 진행하고 !의 불가능 칸 적용하기 + 베이스 캠프 못움직이게 적용하기.
             findBasecamp(k);
-            check = next();
+            // check = next();
+            // if(count == m) check = false;
             k++;
         }
 
@@ -86,9 +89,10 @@ public class Main {
 
     static boolean next(){
         for(int i = 0; i < m; i++){
-            if(!arrive[i]) return true;
+            if(!arrive[i]){ 
+                return true;
+            }
         }
-
         return false;
     }
 
@@ -98,6 +102,7 @@ public class Main {
             if(!arrive[i]){
                 if(peoples[i].x == peoples[i].gx && peoples[i].y == peoples[i].gy){
                     arrive[i] = true;
+                    count++;
                     map[peoples[i].x][peoples[i].y] = -1;
                 }
             }
@@ -146,34 +151,71 @@ public class Main {
         }
     }
 
+    // static void findBasecamp(int number){
+    //     if(number >= m) return;
+    //     int gx = peoples[number].gx;
+    //     int gy = peoples[number].gy;
+
+    //     Queue<int[]> que = new LinkedList<>();
+    //     visited = new boolean[n][n];
+    //     visited[gx][gy] = true;
+    //     que.offer(new int[]{gx, gy});
+
+    //     while(!que.isEmpty()){
+    //         int[] current = que.poll();
+    //         if(map[current[0]][current[1]] == 1){
+    //             map[current[0]][current[1]] = -1;
+    //             peoples[number].x = current[0];
+    //             peoples[number].y = current[1];
+    //             break;
+    //         }
+    //         for(int dir = 0; dir < 4; dir++){
+    //             int nx = current[0] + dx[dir];
+    //             int ny = current[1] + dy[dir];
+    //             if(isRange(nx, ny) && !visited[nx][ny] && map[nx][ny] != -1){
+    //                 visited[nx][ny] = true;
+    //                 que.offer(new int[]{nx, ny});
+    //             }
+    //         }
+    //     }
+    // }
+
     static void findBasecamp(int number){
-        if(number >= m) return;
-        int gx = peoples[number].gx;
-        int gy = peoples[number].gy;
+    if(number >= m) return;
+    int gx = peoples[number].gx;
+    int gy = peoples[number].gy;
 
-        Queue<int[]> que = new LinkedList<>();
-        visited = new boolean[n][n];
-        visited[gx][gy] = true;
-        que.offer(new int[]{gx, gy});
+    PriorityQueue<int[]> que = new PriorityQueue<>((a, b) -> {
+        if(a[2] == b[2]) {
+            if(a[0] == b[0]) return Integer.compare(a[1], b[1]);
+            return Integer.compare(a[0], b[0]);
+        }
+        return Integer.compare(a[2], b[2]);
+    });
+    
+    visited = new boolean[n][n];
+    visited[gx][gy] = true;
+    que.offer(new int[]{gx, gy, 0});
 
-        while(!que.isEmpty()){
-            int[] current = que.poll();
-            if(map[current[0]][current[1]] == 1){
-                map[current[0]][current[1]] = -1;
-                peoples[number].x = current[0];
-                peoples[number].y = current[1];
-                break;
-            }
-            for(int dir = 0; dir < 4; dir++){
-                int nx = current[0] + dx[dir];
-                int ny = current[1] + dy[dir];
-                if(isRange(nx, ny) && !visited[nx][ny] && map[nx][ny] != -1){
-                    visited[nx][ny] = true;
-                    que.offer(new int[]{nx, ny});
-                }
+    while(!que.isEmpty()){
+        int[] current = que.poll();
+        if(map[current[0]][current[1]] == 1){
+            map[current[0]][current[1]] = -1;
+            peoples[number].x = current[0];
+            peoples[number].y = current[1];
+            break;
+        }
+        for(int dir = 0; dir < 4; dir++){
+            int nx = current[0] + dx[dir];
+            int ny = current[1] + dy[dir];
+            if(isRange(nx, ny) && !visited[nx][ny] && map[nx][ny] != -1){
+                visited[nx][ny] = true;
+                que.offer(new int[]{nx, ny, current[2] + 1});
             }
         }
     }
+}
+
 
     static boolean isRange(int nx, int ny){
         return !(nx < 0 || nx >= n || ny < 0 || ny >= n);
