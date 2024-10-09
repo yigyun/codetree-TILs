6661d6c4ce8ff4e33fbe11038470261cde0bf98d
yigyun <70265528+yigyun@users.noreply.github.com>
2024-10-09 18:51:40 +0900

    Title: 꼬리잡기놀이, Time: 93ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 241009/꼬리잡기놀이/tail-catch-play.java
+++ 241009/꼬리잡기놀이/tail-catch-play.java
@@ -98,7 +98,7 @@ public class Main {
                 for(int dir = 0; dir < 4; dir++){
                     int nx = node[0] + dx[dir];
                     int ny = node[1] + dy[dir];
-                    if(isRange(nx, ny) && !visited[nx][ny] && (map[nx][ny] <= 4 && map[nx][ny] > 1)){
+                    if(isRange(nx, ny) && !visited[nx][ny] && (map[nx][ny] < 4 && map[nx][ny] > 1)){
                         if(map[nx][ny] == 2){
                             middle.add(new People(nx, ny));
                         }else if(map[nx][ny] == 3){
@@ -113,10 +113,24 @@ public class Main {
                 }
             }
 
+            que.offer(new int[]{current[0], current[1]});
+            while(!que.isEmpty()){
+                int[] node = que.poll();
+                for(int dir = 0; dir < 4; dir++){
+                    int nx = node[0] + dx[dir];
+                    int ny = node[1] + dy[dir];
+                    if(isRange(nx, ny) && !visited[nx][ny] && map[nx][ny] == 4){
+                        visited[nx][ny] = true;
+                        route.add(new int[]{nx, ny});
+                        teamNumberMap[nx][ny] = teamNum;
+                        que.offer(new int[]{nx, ny});
+                    }
+                }
+            }
+
             teams.add(new Team(head, tail, middle, route));
             teamNum++;
         }
-
 
         for(int t = 0; t < k; t++){
             move();
@@ -146,11 +160,10 @@ public class Main {
             x = 0;
             y = (n - 1) - position;
         }
-
         if(map[x][y] >= 1 && map[x][y] < 4){
             findCheck = false;
         }else{
-            for(int i = 0; i < n; i++){
+            for(int i = 0; i < n - 1; i++){
                 int nx = x + dx[dir];
                 int ny = y + dy[dir];
                 if(isRange(nx, ny)){
@@ -179,13 +192,10 @@ public class Main {
                 }
             }
 
-
             // 방향 바꾸기
             People temp = team.head;
             team.head = team.tail;
-            team.tail = new People(temp.x, temp.y);
-            map[team.head.x][team.head.y] = 1;
-            map[team.tail.x][team.tail.y] = 3;
+            team.tail = temp;
             Collections.reverse(team.middle);
         }
     }
@@ -197,7 +207,6 @@ public class Main {
             // head는 미들이 있으면 미들 반대편, 미들이 없으면 4가 있는 쪽으로 이동하기.
             
             if(team.middle.size() != 0){
-
                 team.tail = team.middle.get(team.middle.size() - 1);
                 
                 team.middle.remove(team.middle.size() - 1);
@@ -213,13 +222,15 @@ public class Main {
                     }
                 }
             }else{
-                team.tail = new People(team.head.x, team.head.y);
+                team.tail.x = team.head.x;
+                team.tail.y = team.head.y;
 
                 for(int dir = 0; dir < 4; dir++){
                     int nx = team.head.x + dx[dir];
                     int ny = team.head.y + dy[dir];
                     if(isRange(nx, ny) && map[nx][ny] != 3 && map[nx][ny] != 0){
-                        team.head = new People(nx, ny);
+                        team.head.x = nx;
+                        team.head.y = ny;
                         break;
                     }
                 }
